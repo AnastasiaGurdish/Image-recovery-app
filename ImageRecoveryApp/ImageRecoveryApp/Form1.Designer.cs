2b28e1258fceb6f3fad22e4d777f194d542b3ad6
Anastasia <43218261+AnastasiaGurdish@users.noreply.github.com>
2021-02-06 18:13:16 +0200

    add project

---
+++ ImageRecoveryApp/ImageRecoveryApp/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace ImageRecoveryApp
+{
+    partial class ImageRecoveryImage
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UploadPhotoButton = new System.Windows.Forms.Button();
+            this.UploadedImage = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UploadedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UploadPhotoButton
+            // 
+            this.UploadPhotoButton.BackColor = System.Drawing.Color.Tomato;
+            this.UploadPhotoButton.FlatAppearance.BorderSize = 0;
+            this.UploadPhotoButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.OrangeRed;
+            this.UploadPhotoButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            this.UploadPhotoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UploadPhotoButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UploadPhotoButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UploadPhotoButton.Location = new System.Drawing.Point(45, 62);
+            this.UploadPhotoButton.Name = "UploadPhotoButton";
+            this.UploadPhotoButton.Size = new System.Drawing.Size(342, 48);
+            this.UploadPhotoButton.TabIndex = 0;
+            this.UploadPhotoButton.Text = "Загрузить фото";
+            this.UploadPhotoButton.UseVisualStyleBackColor = false;
+            // 
+            // UploadedImage
+            // 
+            this.UploadedImage.BackColor = System.Drawing.Color.MistyRose;
+            this.UploadedImage.Location = new System.Drawing.Point(586, 27);
+            this.UploadedImage.Name = "UploadedImage";
+            this.UploadedImage.Size = new System.Drawing.Size(469, 602);
+            this.UploadedImage.TabIndex = 1;
+            this.UploadedImage.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.checkBox1.Location = new System.Drawing.Point(45, 397);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(367, 27);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Первый способ восстановления";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.checkBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.checkBox2.Location = new System.Drawing.Point(45, 451);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(363, 27);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Второй способ восстановления";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.checkBox3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.checkBox3.Location = new System.Drawing.Point(45, 507);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(358, 27);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Третий способ восстановления";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.OrangeRed;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(45, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(342, 48);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Наложить искажение";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Tomato;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.OrangeRed;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(45, 571);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(342, 48);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Восстановить изображение";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.checkBox4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.checkBox4.Location = new System.Drawing.Point(45, 252);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(285, 27);
+            this.checkBox4.TabIndex = 9;
+            this.checkBox4.Text = "Искажаюющая функция";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.checkBox5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.checkBox5.Location = new System.Drawing.Point(45, 196);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(189, 27);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "Наложить смаз";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.checkBox6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.checkBox6.Location = new System.Drawing.Point(45, 142);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(184, 27);
+            this.checkBox6.TabIndex = 7;
+            this.checkBox6.Text = "Наложить шум";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // ImageRecoveryImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.UploadedImage);
+            this.Controls.Add(this.UploadPhotoButton);
+            this.Name = "ImageRecoveryImage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Реставратор фото";
+            ((System.ComponentModel.ISupportInitialize)(this.UploadedImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UploadPhotoButton;
+        private System.Windows.Forms.PictureBox UploadedImage;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+    }
+}
+
